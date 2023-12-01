d0adf66ac6af1c3f992e77998a7e997bb3c07e6c
Kenn1476 <97831182+Kenn1476@users.noreply.github.com>
2023-12-01 23:12:31 +0900

    ユーザー commit 3

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Internet.password(min_length: 6, mix_case: true)}
     password_confirmation {password}
+    last_name             {'ナギット'}
+    first_name            {'ケネット'}
+    last_name_kana        {'ナギット'}
+    first_name_kana       {'ケネット'}
+    birth_date            {Faker::Date.birthday}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "全ての項目が入力されていれば登録できる" do
+        expect(@user).to be_valid
+      end
+    end
+
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
@@ -16,27 +22,99 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include "Email can't be blank"
     end
     it "passwordが空では登録できない" do
-      @user.password = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
-    end
-    it 'passwordが5文字以下では登録できない' do
-      @user.password = '12345'
-      @user.password_confirmation = '12345'
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
-    end
-    it 'passwordが129文字以上では登録できない' do
-      @user.password =  Faker::Internet.password(min_length: 129, max_length: 150)
-      @user.password_confirmation =  @user.password
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
-    end
-    it 'passwordとpassword_confirmationが不一致では登録できない' do
-      @user.password = '123456'
-      @user.password_confirmation = '1234567'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-    end
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '12345'
+        @user.password_confirmation = '12345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+      it 'passwordが129文字以上では登録できない' do
+        @user.password =  Faker::Internet.password(min_length: 129, max_length: 150)
+        @user.password_confirmation =  @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it '数字のみのパスワードでは登録できない' do
+        @user.password = '111222333'
+        @user.password_confirmation = '111222333'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password password must contain both letters and numbers.")
+      end
+      it '数字のみのパスワードでは登録できない' do
+        @user.password = 'kkklllmmm'
+        @user.password_confirmation = 'kkklllmmm'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password password must contain both letters and numbers.")
+      end
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'たなか'
+        @user.password_confirmation = 'たなか'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password password must contain both letters and numbers.")
+      end
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'たなか'
+        @user.password_confirmation = 'たなか'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password password must contain both letters and numbers.")
+      end
+
+
+      it '姓（全角）が空だと登録できない' do
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
+      end
+      it '姓（全角）に半角文字が含まれていると登録できない' do
+        @user.last_name = 'ｱ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid. Input full-width characters.")
+      end
+      it '名（全角）が空だと登録できない' do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+      it '名（全角）に半角文字が含まれていると登録できない' do
+        @user.first_name = 'ｱ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid. Input full-width characters.")
+      end
+      it '姓（カナ）が空だと登録できない' do
+        @user.last_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+      end
+      it '姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        @user.last_name_kana = 'なぎっと'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid. Input full-width katakana characters.")
+      end
+      it '名（カナ）が空だと登録できない' do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end
+      it '名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid. Input full-width katakana characters.")
+      end
+
+      it '生年月日が空だと登録できない' do
+        @user.birth_date = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birth date can't be blank")
+      end
   end
 end
