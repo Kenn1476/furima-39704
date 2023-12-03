ce0ab685f9abbd43b96b1382ac77810e45168c8c
Kenn1476 <97831182+Kenn1476@users.noreply.github.com>
2023-12-03 15:10:37 +0900

    商品出品 commit 6

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,9 +2,8 @@ const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
    const inputValue = priceInput.value;
    const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
     const addProfit = document.getElementById("profit");
     addProfit.innerHTML = Math.floor(inputValue * 0.9);
-    
 })
 
