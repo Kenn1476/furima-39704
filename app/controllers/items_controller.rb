e53435334e7a7645bd0dd3937d354ac9e1dc7b69
Kenn1476 <97831182+Kenn1476@users.noreply.github.com>
2023-12-06 15:31:02 +0900

    商品削除 commit 3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,8 +24,6 @@ class ItemsController < ApplicationController
     if @item.user_id == current_user.id
       @item.destroy
       redirect_to root_path
-    else
-      redirect_to root_path
     end
   end
 
