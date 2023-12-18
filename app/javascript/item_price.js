1cd0882d98ada9e0412275e7e2fa9ae8e367ba7e
Kenn1476 <97831182+Kenn1476@users.noreply.github.com>
2023-12-18 20:50:33 +0900

    Fix JS/ Fix Image

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,9 +1,12 @@
-const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", () => {
-   const inputValue = priceInput.value;
-   const addTaxDom = document.getElementById("add-tax-price");
+window.addEventListener('load', () => {
+    const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
     const addProfit = document.getElementById("profit");
     addProfit.innerHTML = Math.floor(inputValue * 0.9);
-})
+    })
+    
+});
 
